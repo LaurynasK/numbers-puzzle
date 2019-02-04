254eb04c7b9b9fcedd7bac5d04b536674638ce60
LaurynasK <enemy911@gmail.com>
2019-02-04 13:25:09 +0200

    small changes + project description

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Table = require('table');
+const puzzle = require("./lib-puzzle/puzzle");
+
 
 const printTable = (array) => console.log(Table.table(array));
-const puzzle = require("./lib-puzzle/puzzle");
 
 puzzle.Puzzle(4, printTable);
